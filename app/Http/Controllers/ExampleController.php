0896b9ab5fb45462eeb0f2eaec2e0bb499f1bd8d
boo <boo@boo.xx>
2021-05-15 16:31:20 +0200

    Controller

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -1,18 +1,17 @@
 <?php
-
 namespace App\Http\Controllers;
 
 class ExampleController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * Retrieve the user for the given ID.
+     * $router->get('user/{id}', 'ExampleController@show');
      *
-     * @return void
+     * @param  int  $id
+     * @return Response
      */
-    public function __construct()
+    public function show($id)
     {
-        //
+        return "User id: " . $id;
     }
-
-    //
 }
